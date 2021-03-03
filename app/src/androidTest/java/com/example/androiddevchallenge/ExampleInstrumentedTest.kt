473af690045bdba061a6c361084ed11d6131c864
huangyonghuang <huangyonghuang@jze100.com>
2021-03-03 14:30:52 +0800

    update

--- app/src/androidTest/java/com/example/androiddevchallenge/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/androiddevchallenge/ExampleInstrumentedTest.kt
@@ -17,7 +17,7 @@ package com.example.androiddevchallenge
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.f1reking.androiddevchallenge.MainActivity
+import com.f1reking.androiddevchallenge.dogadoption.MainActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
